8654828f9ec1e555a633ca4d79e2dd30f51df73b
b <ritumkasov>
2024-07-26 18:48:04 +0300

    Added enums

---
+++ src/main/java/com/mochawake/enums/AchievementName.java
@@ -0,0 +1,7 @@
+package com.mochawake.enums;
+
+public enum AchievementName {
+    BEGINNER_COOK,
+    MASTER_CHEF,
+    RECIPE_INNOVATOR
+}
---
+++ src/main/java/com/mochawake/enums/TypeName.java
@@ -0,0 +1,16 @@
+package com.mochawake.enums;
+
+public enum TypeName {
+    ESPRESSO,
+    DOPPIO,
+    AMERICANO,
+    CAPPUCCINO,
+    LATTE,
+    MOCHA,
+    CORTADO,
+    MACCHIATO,
+    CON_PANNA,
+    AFFOGATO,
+    IRISH_COFFEE
+
+}
